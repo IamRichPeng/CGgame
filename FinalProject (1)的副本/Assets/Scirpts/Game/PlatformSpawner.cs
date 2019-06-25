657fde1dae355119eeb160eea28672b9ed1e5f99
pradawa <rpby6@mail.missouri.edu>
2019-06-25 17:02:12 -0500

    finished CG project
    Computer Graphic project using Unity

---
+++ FinalProject (1)的副本/Assets/Scirpts/Game/PlatformSpawner.cs
@@ -0,0 +1,289 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlatformGroupType
+{
+    Grass,
+    Winter
+}
+public class PlatformSpawner : MonoBehaviour
+{
+    public Vector3 startSpawnPos;
+    public int milestoneCount = 10;
+    public float fallTime;
+    public float minFallTime;
+    public float multiple;
+    private ManagerVars vars;
+    // number of platforms (5)
+    private int spawnPlatformCount;
+    //platform left or right
+    private bool isLeftSpawn = false;
+    // location of generating new platform
+    private Vector3 platformSpawnPosition;
+    private Sprite selectPlatformSprite;
+    // platform type: winter or grass
+    private PlatformGroupType groupType;
+    // for generate more platforms in spike direction
+    private bool spikeSpawnLeft = false;
+    private Vector3 spikeDirPlatformPos;
+    private int afterSpawnSpikeSpawnCount;
+    private bool isSpawnSpike;
+    private void Awake()
+    {
+        EventCenter.AddListener(EventDefine.DecidePath, DecidePath);
+        vars = ManagerVars.GetManagerVars();
+    }
+    private void OnDestroy()
+    {
+        EventCenter.RemoveListener(EventDefine.DecidePath, DecidePath);
+    }
+
+    private void Start()
+    {
+        RandomPlatformTheme();
+        platformSpawnPosition = startSpawnPos;
+        for (int i = 0; i < 5; i++)
+        {
+            spawnPlatformCount = 5;
+            DecidePath();
+        }
+
+        // generate character.
+        GameObject go = Instantiate(vars.characterPre);
+        go.transform.position = new Vector3(0, -1.8f, 0);
+    }
+
+    private void Update()
+    {
+        if (GameManager.Instance.IsGameStarted && GameManager.Instance.IsGameOver == false)
+        {
+            UpdateFallTime();
+        }
+    }
+    private void UpdateFallTime()
+    {
+        if (GameManager.Instance.GetGameScore() > milestoneCount)
+        {
+            milestoneCount *= 2;
+            fallTime *= multiple;
+            if (fallTime < minFallTime)
+            {
+                fallTime = minFallTime;
+            }
+
+        }
+    }
+
+    private void DecidePath()
+    {
+        // dont need to repeated decidepath, if the platform has spike, just invoke specific func.
+        if (isSpawnSpike)
+        {
+            AfterSpawnSpike();
+            return;
+        }
+        if (spawnPlatformCount > 0)
+        {
+            spawnPlatformCount--;
+            SpawnPlatform();
+        }
+        else
+        {
+            isLeftSpawn = !isLeftSpawn;
+            spawnPlatformCount = Random.Range(1, 4);
+            SpawnPlatform();
+        }
+    }
+
+    private void SpawnPlatform()
+    {
+        int ranObstacleDir = Random.Range(0, 2);
+        if (spawnPlatformCount >= 1)
+        {
+            SpawnNormalPlatform(ranObstacleDir);
+        }
+        else if(spawnPlatformCount == 0)
+        {
+            int ran = Random.Range(0, 3);
+            if(ran == 0)
+            {
+                SpawnCommonPlatformGroup(ranObstacleDir);
+            }
+            else if(ran == 1)
+            {
+                switch (groupType)
+                {
+                    case PlatformGroupType.Grass:
+                        SpawnGrassPlatformGroup(ranObstacleDir);
+                        break;
+                    case PlatformGroupType.Winter:
+                        SpawnWinterPlatformGroup(ranObstacleDir);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // generate platform with spike
+            else
+            {
+                int value = -1;
+                if (isLeftSpawn)
+                {
+                    value = 0;
+                }
+                else
+                {
+                    value = 1;
+                }
+                SpawnSpikePlatform(value);
+
+                isSpawnSpike = true;
+                // number of extended platforms
+                afterSpawnSpikeSpawnCount = 4;
+                if (spikeSpawnLeft) // generate more platform in left direction
+                {
+                    spikeDirPlatformPos = new Vector3(platformSpawnPosition.x - 1.65f,
+                        platformSpawnPosition.y + vars.nextYPos, 0);
+                }
+                else // generate platform in right
+                {
+                    spikeDirPlatformPos = new Vector3(platformSpawnPosition.x + 1.65f,
+                        platformSpawnPosition.y + vars.nextYPos, 0);
+                }
+            }
+        }
+
+        int ranSpawnDiamond = Random.Range(0, 8);
+        if (ranSpawnDiamond >= 6 && GameManager.Instance.PlayerIsMove)
+        {
+            GameObject go = ObjectPool.Instance.GetDiamond();
+            go.transform.position = new Vector3(platformSpawnPosition.x,
+                platformSpawnPosition.y + 0.5f, 0);
+            go.SetActive(true);
+        }
+        if (isLeftSpawn)
+        {
+            platformSpawnPosition = new Vector3(platformSpawnPosition.x - vars.nextXPos,
+                platformSpawnPosition.y + vars.nextYPos, 0);
+        }
+        else
+        {
+            platformSpawnPosition = new Vector3(platformSpawnPosition.x + vars.nextXPos,
+                platformSpawnPosition.y + vars.nextYPos, 0);
+        }
+    }
+    private void RandomPlatformTheme()
+    {
+        int ran = Random.Range(0, vars.platformThemeSpriteList.Count);
+        selectPlatformSprite = vars.platformThemeSpriteList[ran];
+
+        if (ran == 2)
+        {
+            groupType = PlatformGroupType.Winter;
+        }
+        else
+        {
+            groupType = PlatformGroupType.Grass;
+        }
+    }
+
+
+    private void SpawnNormalPlatform(int ranObstacleDir)
+    {
+        GameObject go = ObjectPool.Instance.GetNormalPlatform();
+        go.transform.position = platformSpawnPosition;
+        go.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, ranObstacleDir);
+        go.SetActive(true);
+    }
+    private void SpawnCommonPlatformGroup(int ranObstacleDir)
+    {
+        GameObject go = ObjectPool.Instance.GetCommonPlatformGroup();
+        go.transform.position = platformSpawnPosition;
+        go.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, ranObstacleDir);
+        go.SetActive(true);
+    }
+    private void SpawnGrassPlatformGroup(int ranObstacleDir)
+    {
+        GameObject go = ObjectPool.Instance.GetGrassPlatformGroup();
+        go.transform.position = platformSpawnPosition;
+        go.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, ranObstacleDir);
+        go.SetActive(true);
+    }
+    private void SpawnWinterPlatformGroup(int ranObstacleDir)
+    {
+        GameObject go = ObjectPool.Instance.GetWinterPlatformGroup();
+        go.transform.position = platformSpawnPosition;
+        go.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, ranObstacleDir);
+        go.SetActive(true);
+    }
+
+    // generate spike platform input dierction
+    private void SpawnSpikePlatform(int dir)
+    {
+        GameObject temp = null;
+        if(dir == 0)
+        {
+            spikeSpawnLeft = false;
+            temp = ObjectPool.Instance.GetRightSpikePlatform();
+        }
+        else
+        {
+            spikeSpawnLeft = true;
+            temp = ObjectPool.Instance.GetLeftSpikePlatform();
+        }
+        temp.transform.position = platformSpawnPosition;
+        temp.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, dir);
+        temp.SetActive(true);
+    }
+
+    // generate platforms after spikeplatform, including both directions: spike and original
+    private void AfterSpawnSpike()
+    {
+        if (afterSpawnSpikeSpawnCount > 0)
+        {
+            afterSpawnSpikeSpawnCount--;
+            for (int i = 0; i < 2; i++)
+            {
+                GameObject temp = ObjectPool.Instance.GetNormalPlatform();
+                if (i == 0) //original direction, renew next platform location
+                {
+                    temp.transform.position = platformSpawnPosition;
+                    if (spikeSpawnLeft)
+                    {
+                        platformSpawnPosition = new Vector3(platformSpawnPosition.x + vars.nextXPos,
+                            platformSpawnPosition.y + vars.nextYPos, 0);
+                    }
+                    else
+                    {
+                        platformSpawnPosition = new Vector3(platformSpawnPosition.x - vars.nextXPos,
+                            platformSpawnPosition.y + vars.nextYPos, 0);
+                    }
+                }
+                else// spike direction
+                {
+                    temp.transform.position = spikeDirPlatformPos;
+                    if (spikeSpawnLeft)
+                    {
+                        spikeDirPlatformPos = new Vector3(spikeDirPlatformPos.x - vars.nextXPos,
+                            spikeDirPlatformPos.y + vars.nextYPos, 0);
+                    }
+                    else
+                    {
+                        spikeDirPlatformPos = new Vector3(spikeDirPlatformPos.x + vars.nextXPos,
+                            spikeDirPlatformPos.y + vars.nextYPos, 0);
+                    }
+                }
+
+                temp.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, 1);
+                temp.SetActive(true);
+            }
+        }
+        else
+        {
+            isSpawnSpike = false;
+            DecidePath();
+        }
+    }
+}
---
+++ FinalProject (1)的副本/Assets/Scirpts/Game/PlayerController.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform rayDown, rayLeft, rayRight;
+    public LayerMask platformLayer, obstacleLayer;
+    private bool isMoveLeft = false;
+    private Vector3 nextPlatformLeft, nextPlatformRight;
+    // to stop infinite jumping, see if character is jumping
+    private bool isJumping = false;
+    private bool isMove = false;
+    private ManagerVars vars;
+    private Rigidbody2D my_Body;
+    private SpriteRenderer spriteRenderer;
+    private AudioSource m_AudioSource;
+    private void Awake()
+    {
+        EventCenter.AddListener<bool>(EventDefine.IsMusicOn, IsMusicOn);
+        EventCenter.AddListener<int>(EventDefine.ChangeSkin, ChangeSkin);
+        vars = ManagerVars.GetManagerVars();
+        my_Body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+    private void Start()
+    {
+        ChangeSkin(GameManager.Instance.GetCurrentSelectedSkin());
+    }
+    private void OnDestroy()
+    {
+        EventCenter.RemoveListener<int>(EventDefine.ChangeSkin, ChangeSkin);
+        EventCenter.RemoveListener<bool>(EventDefine.IsMusicOn, IsMusicOn);
+    }
+    private void ChangeSkin(int skinIndex)
+    {
+        spriteRenderer.sprite = vars.characterSkinSpriteList[skinIndex];
+    }
+    private void IsMusicOn(bool value)
+    {
+        m_AudioSource.mute = !value;
+    }
+    private bool IsPointerOverGameObject(Vector2 mousePosition)
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = mousePosition;
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raycastResults);
+        return raycastResults.Count > 0;
+    }
+    private void Update()
+    {
+        if (IsPointerOverGameObject(Input.mousePosition)) return;
+        if (GameManager.Instance.IsGameStarted == false || GameManager.Instance.IsGameOver || GameManager.Instance.IsPause)
+            return;
+        if (Input.GetMouseButtonDown(0) && isJumping == false && nextPlatformLeft != Vector3.zero)
+        {
+            if (isMove == false)
+            {
+                EventCenter.Broadcast(EventDefine.PlayerMove);
+                isMove = true;
+            }
+            m_AudioSource.PlayOneShot(vars.jumpClip);
+            EventCenter.Broadcast(EventDefine.DecidePath);
+            isJumping = true;
+
+            //Determine click right or left side of the screen.
+            Vector3 mousePos = Input.mousePosition;
+            if (mousePos.x <= Screen.width / 2)
+            {
+                isMoveLeft = true;
+            }
+            else if (mousePos.x > Screen.width / 2)
+            {
+                isMoveLeft = false;
+            }
+            Jump();
+        }
+        
+        //Game Over 1
+        if (my_Body.velocity.y < 0 && IsRayPlatform() == false && GameManager.Instance.IsGameOver == false)
+        {
+            m_AudioSource.PlayOneShot(vars.fallClip);
+            spriteRenderer.sortingLayerName = "Default";
+            GetComponent<BoxCollider2D>().enabled = false;
+            GameManager.Instance.IsGameOver = true;
+            StartCoroutine(DealyShowGameOverPanel());
+        }
+        //Game Over 2
+        if (isJumping && IsRayObstacle() && GameManager.Instance.IsGameOver == false)
+        {
+            m_AudioSource.PlayOneShot(vars.hitClip);
+            GameObject go = ObjectPool.Instance.GetDeathEffect();
+            go.SetActive(true);
+            go.transform.position = transform.position;
+            GameManager.Instance.IsGameOver = true;
+            spriteRenderer.enabled = false;
+            StartCoroutine(DealyShowGameOverPanel());
+        }
+        //Game over 3
+        if (transform.position.y - Camera.main.transform.position.y < -5 && GameManager.Instance.IsGameOver == false)
+        {
+            m_AudioSource.PlayOneShot(vars.fallClip);
+            GameManager.Instance.IsGameOver = true;
+            StartCoroutine(DealyShowGameOverPanel());
+        }
+    }
+    IEnumerator DealyShowGameOverPanel()
+    {
+        yield return new WaitForSeconds(1f);
+        EventCenter.Broadcast(EventDefine.ShowGameOverPanel);
+    }
+    private void Jump()
+    {
+        if (isMoveLeft)
+        {
+            //character facing direction
+            transform.localScale = new Vector3(-1, 1, 1);
+            transform.DOMoveX(nextPlatformLeft.x, 0.2f);
+            transform.DOMoveY(nextPlatformLeft.y + 0.8f, 0.15f);
+        }
+        else
+        {
+            transform.DOMoveX(nextPlatformRight.x, 0.2f);
+            transform.DOMoveY(nextPlatformRight.y + 0.8f, 0.15f);
+            transform.localScale = Vector3.one;
+        }
+    }
+    //avoid addscore in the same platform
+    private GameObject lastHitGo = null;
+    // detect platform
+    private bool IsRayPlatform()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rayDown.position, Vector2.down, 1f, platformLayer);
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "Platform")
+            {
+                if (lastHitGo != hit.collider.gameObject)
+                {
+                    if (lastHitGo == null)
+                    {
+                        lastHitGo = hit.collider.gameObject;
+                        return true;
+                    }
+                    // broadcast addscore
+                    EventCenter.Broadcast(EventDefine.AddScore);
+                    lastHitGo = hit.collider.gameObject;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+    //detect obstacle
+    private bool IsRayObstacle()
+    {
+        RaycastHit2D leftHit = Physics2D.Raycast(rayLeft.position, Vector2.left, 0.15f, obstacleLayer);
+        RaycastHit2D rightHit = Physics2D.Raycast(rayRight.position, Vector2.right, 0.15f, obstacleLayer);
+
+        if (leftHit.collider != null)
+        {
+            if (leftHit.collider.tag == "Obstacle")
+            {
+                return true;
+            }
+        }
+
+        if (rightHit.collider != null)
+        {
+            if (rightHit.collider.tag == "Obstacle")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    // see if the character collide platform
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Platform")
+        {
+            isJumping = false;
+            Vector3 currentPlatformPos = collision.gameObject.transform.position;
+            nextPlatformLeft = new Vector3(currentPlatformPos.x -
+                vars.nextXPos, currentPlatformPos.y + vars.nextYPos, 0);
+            nextPlatformRight = new Vector3(currentPlatformPos.x +
+                vars.nextXPos, currentPlatformPos.y + vars.nextYPos, 0);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Pickup")
+        {
+            m_AudioSource.PlayOneShot(vars.diamondClip);
+            EventCenter.Broadcast(EventDefine.AddDiamond);
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
